fa67f8ba06075525dc8d6b62984780b42a4c458f
Mike <contact@mikecodeur.com>
2021-03-04 18:06:19 +0800

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -13,5 +13,11 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 }); 
 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+}); 
+
 
 //
